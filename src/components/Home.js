ba4de556ef89128a3289d6234d6100a258e145b8
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-07 15:36:01 +0100

    Fourth commit

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import heroImage from 'asset';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h2>
+                I'm a Full Stack Developer
+            </h2>
+            <p>
+                lorem
+            </p>
+
+            <div>
+                <button>
+                    Portfolio<span>
+                    <MdOutlineKeyboardArrowRight />
+                    </span>
+                </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
