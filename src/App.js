64159a6f44199744155015e22ac3069212814702
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-07 09:42:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Navbar from "./components/navabar";
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">Adeboss Web</h1>
+       <Navbar />
     </div>
+   
   );
 }
 
