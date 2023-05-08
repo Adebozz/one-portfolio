22af32cd05f41484d59bc866a364f484bd9ecaed
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-08 09:28:40 +0100

    social commit

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
-import {HiOutlineMail} from 'react-icons/bs';
+import {HiOutlineMail} from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 const SocialLinks = () => {
 
@@ -22,7 +22,7 @@ const SocialLinks = () => {
                 Github <FaGithub size={30}/>
                 </>
             ),
-            href: 'https//github.com/Adebozz',
+            href: 'https://github.com/Adebozz',
             
         },
         {
@@ -49,20 +49,22 @@ const SocialLinks = () => {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-14 
-        px-4 ml-[-100px] hover:ml-[-10px] hoer:rounded-md
-        duration-300 bg-gray-500'>
-            <a 
-            href=''
-            className='flex justify-between items-center w-full
-            text-white'
-            >
-            <>
-            LinkedIn <FaLinkedin size={30}/>
-            
-            </>
-            </a>
-            </li>
+        {links.map(({id, child, href, style, download}) => (
+                <li key={id} 
+                className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hoer:rounded-md duration-300 bg-gray-500' + '' + style}
+                >
+                    <a 
+                    href= {href}
+                    className='flex justify-between items-center w-full
+                    text-white'
+                    download={download}
+                    target='_blank'
+                    >
+                        {child}
+                    </a>
+                    </li>
+        ))}
+        
       </ul>
     </div>
 )
