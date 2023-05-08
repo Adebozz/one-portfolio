be6c3dce32ba0300ac478beb320a07e4686d9c60
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-08 10:32:35 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Navbar from "./components/navabar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/about";
+
 
 function App() {
   return (
     <div>
        <Navbar />
        <Home />
+       <About />
 
        <SocialLinks />
+       
     </div>
    
   );
--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
-const about = () => {
+const About = () => {
   return (
-    <div>
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       
+<div className='max-w-screen-lg p-4 mx-auto flex flex-col
+justify-center w-full h-full'>
+    <div className='pb-8'>
+        <p className='text-4xl font-bold inline border-b-4
+        border-gray-500'>About</p>
+    </div>
+    <p className='text-xl mt-20'> 
+    Lorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolar
+    </p>
+    <br />
+    <p className='text-xl'>
+    Lorem ipsum dolarLorem ipsum dolarvvvvvvvvLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolarLorem ipsum dolar
+    </p>
+</div>
+
     </div>
   )
 }
 
-export default about
+export default About
