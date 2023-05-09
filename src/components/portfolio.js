f09f1081db981d5d8cadc5408615596b4b77e2ce
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-09 16:09:19 +0100

    fourth commit

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -26,24 +26,30 @@ const Portfolio = () => {
           </p>
           <p className="py-6">Check out some fo my works right here</p>
         </div>
-        <div
-          className="grid sm:grid-cols-2 md:grid-cols-3 gap-8
-        px-12 sm:px-0"
-        >
-          <div className="shadow-md shadow-gray-600 rounded-lg">
-            <img
-              src=""
-              alt=""
-              className="rounded-md duration-200 hover:scale-105"
-            />
-            <div className="flex items-center justify-center">
-              <button className="w-1/2 px-6 py-3 m-4 duration-200
-              hover: scale">Demo</button>
-              <button className="w-1/2 px-6 py-3 m-4 duration-200
-              hover: scale">Code</button>
+        
+           <div
+            className="grid sm:grid-cols-2 md:grid-cols-3 gap-8
+          px-12 sm:px-0"
+          >
+            {
+          portfolio.map(({id, src}) => {
+            <div className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src=""
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200
+                hover: scale">Demo</button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200
+                hover: scale">Code</button>
+              </div>
             </div>
+          })
+        }
+            
           </div>
-        </div>
       </div>
     </div>
   );
