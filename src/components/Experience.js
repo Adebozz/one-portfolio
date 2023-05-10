98faa84bc2866febe2fd0592797b3c09ea879135
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-10 07:34:58 +0100

    fifth commit

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,24 +1,30 @@
-import React from 'react'
-
+import React from "react";
 
 const Experience = () => {
   return (
-    <div>
-      <div>
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full
+    h-screen"
+    >
+      <div
+        className="max-w-screen-lg mx-auto p-4 flex flex-col
+      justify-center w-full h-full text-white"
+      >
         <div>
-            <p>Experience</p>
-            <p>This are the technologies i have worked with</p>
+          <p>Experience</p>
+          <p>This are the technologies i have worked with</p>
         </div>
 
         <div>
-            <div>
-                <img src='' alt='' />
-                <p></p>
-            </div>
+          <div>
+            <img src="" alt="" />
+            <p>Html</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
